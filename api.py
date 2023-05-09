8befe9734840e6471504eb09354326472205ea73
olega211 <106370448+olega211@users.noreply.github.com>
2023-05-09 17:17:16 +0700

    new structure

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 import requests
 API_DOMAIN = 'http://voxys.meatqwtest.ga'
+# API_DOMAIN = 'http://127.0.0.1:8000'
 
 def post_client(client):
     response = requests.post(API_DOMAIN + '/api/client', client)
-    
-    if response.status_code == 201:
+    if response.status_code in [200, 201]:
         return response.json()['data']['id']
     else:
         return None
@@ -12,8 +12,16 @@ def post_client(client):
 def post_meeting(data):
     
     response = requests.post(API_DOMAIN + '/api/meeting', data)
-    
-    if (response.status_code == 201):
+    if response.status_code in [200, 201]:
         return True
     else:
         return False
+    
+def get_information():
+    
+    response = requests.get(API_DOMAIN + '/api/information')
+    if response.status_code == 200:
+        return response.json()['data']
+    else:
+        return False
+    
--- bot.py
+++ bot.py
@@ -19,20 +19,59 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 # form meetingForm
+
+
 class meetingForm(StatesGroup):
     name = State()
     phone = State()
     date = State()
     time = State()
-    
+
+
+# main keyboard
+main_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+main_keyboard.row('Записаться 📝',
+                  'Информация 📖')
+
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     """START HANDLING"""
 
+    await bot.send_message(message.chat.id,
+                           'Привет 👋, здесь ты можешь получить информацию о компании и вакансиях и записаться на собеседование',
+                           reply_markup=main_keyboard)
+
+
+@dp.message_handler(Text(equals='Записаться 📝'))
+async def function_sign_up(message: types.Message):
+    """Отработка нажатия ЗАПИСАТЬСЯ"""
+
     # start fullname state
     await meetingForm.name.set()
-    await bot.send_message(message.chat.id, 'Привет, здесь ты можешь записаться на собеседование')
     await bot.send_message(message.chat.id, 'Как тебя зовут')
+    
+    
+@dp.message_handler(Text(equals='Информация 📖'))
+async def function_information(message: types.Message):
+    """Отработка нажатия ИНФОРМАЦИЯ"""
+
+    information = get_information()
+    if information:
+        for item in information:
+            await bot.send_message(
+                message.chat.id,
+                md.text(
+                    md.text(md.bold(item['title'])),
+                    md.text(item['text']),
+                    sep='\n',
+                ),
+                parse_mode=ParseMode.MARKDOWN,
+            )
+            
+    else:
+        await bot.send_message(message.chat.id, 'Что то пошло не так, попробуйте еще раз 🥺')
+
 
 @dp.message_handler(state=meetingForm.name)
 async def process_name(message: types.Message, state: FSMContext):
@@ -43,12 +82,30 @@ async def process_name(message: types.Message, state: FSMContext):
 
     # start phone state
     await meetingForm.next()
-    await bot.send_message(message.chat.id, "Введи свой номер телефона в формате\n8-XXX-XXX-XX-XX")
+    await bot.send_message(message.chat.id, "☎️ Введи свой номер телефона в формате\n8-XXX-XXX-XX-XX")
 
 # проверка маски телефона
+
+
 @dp.message_handler(lambda message: bool(re.match(r"^8-\d{3}-\d{3}-\d{2}-\d{2}$", message.text)) != True, state=meetingForm.phone)
 async def process_phone_invalid(message: types.Message):
-    return await message.reply('Не валидный формат, введи в формате 8-XXX-XXX-XX-XX')    
+    return await message.reply('❌ Не валидный формат, введи в формате 8-XXX-XXX-XX-XX')
+
+
+def render_date_buttons():
+    date_keyboard = types.InlineKeyboardMarkup(
+        resize_keyboard=True, selective=True)
+
+
+    date_btn_today = types.InlineKeyboardButton(
+        'Сегодня', callback_data='date_btn_today')
+    date_btn_tomorrow = types.InlineKeyboardButton(
+        'Завтра', callback_data='date_btn_tomorrow')
+    date_btn_day_after_tomorrow = types.InlineKeyboardButton(
+        'Послезавтра', callback_data='date_btn_day_after_tomorrow')
+    date_keyboard.add(date_btn_today, date_btn_tomorrow, date_btn_day_after_tomorrow)
+    
+    return date_keyboard
 
 
 @dp.message_handler(lambda message: bool(re.match(r"^8-\d{3}-\d{3}-\d{2}-\d{2}$", message.text)), state=meetingForm.phone)
@@ -60,62 +117,82 @@ async def process_phone(message: types.Message, state: FSMContext):
 
     # start date state
     await meetingForm.next()
-    await bot.send_message(message.chat.id, 'Введи дату в формате ДД.ММ.ГГГГ')
+    await bot.send_message(message.chat.id, '📅 Выбери удобную дату', reply_markup=render_date_buttons())
+
+
+
+def render_time_buttons():
+    time_keyboard = types.InlineKeyboardMarkup(
+        resize_keyboard=True, selective=True)
+
+
+    time_btn_one = types.InlineKeyboardButton(
+        '12:00', callback_data='time_btn_12:00')
+    time_btn_two = types.InlineKeyboardButton(
+        '15:00', callback_data='time_btn_15:00')
+    time_btn_three = types.InlineKeyboardButton(
+        '18:00', callback_data='time_btn_18:00')
+    time_keyboard.add(time_btn_one, time_btn_two, time_btn_three)
     
-
-# проверка маски даты
-@dp.message_handler(lambda message: bool(re.match(r"^\d{2}\.\d{2}\.\d{4}$", message.text)) != True, state=meetingForm.date)
-async def process_date_invalid(message: types.Message):
-    return await message.reply('Не валидный формат, введи в формате ДД.ММ.ГГГГ') 
+    return time_keyboard
 
 
-@dp.message_handler(lambda message: bool(re.match(r"^\d{2}\.\d{2}\.\d{4}$", message.text)), state=meetingForm.date)
-async def process_date(message: types.Message, state: FSMContext):
-    """DATE STATE"""
+@dp.callback_query_handler(Text(startswith="date_btn_"), state=meetingForm.date)
+async def callbacks_process_date(call: types.CallbackQuery, state: FSMContext):
+    """CALLBACK DATE"""
+    action = call.data.split('date_btn_')[1]
 
+    if action == "today":
+        date = datetime.datetime.today().date()
+    elif action == 'tomorrow':
+        date = (datetime.datetime.today() + datetime.timedelta(days=1)).date()
+    elif action == 'day_after_tomorrow':
+        date = (datetime.datetime.today() + datetime.timedelta(days=2)).date()
+        
     async with state.proxy() as data:
-        data['date'] = message.text
-
-    # start time state
+        data['date'] = date
+    
     await meetingForm.next()
-    await bot.send_message(message.chat.id, 'Введи время в формате ЧЧ:ММ')
+    await bot.send_message(call.message.chat.id, '🕐 Выбери удобное время', reply_markup=render_time_buttons())
+        
 
-# проверка маски времени
-@dp.message_handler(lambda message: bool(re.match(r"^\d{2}\:\d{2}$", message.text)) != True, state=meetingForm.time)
-async def process_time_invalid(message: types.Message):
-    return await message.reply('Не валидный формат, введи в формате ЧЧ:ММ') 
-
-@dp.message_handler(lambda message: bool(re.match(r"^\d{2}\:\d{2}$", message.text)), state=meetingForm.time)
-async def process_time(message: types.Message, state: FSMContext):
-    """TIME STATE"""
-
+@dp.callback_query_handler(Text(startswith="time_btn_"), state=meetingForm.time)
+async def callbacks_process_time(call: types.CallbackQuery, state: FSMContext):
+    """CALLBACK TIME"""
+    action = call.data.split('time_btn_')[1]
+        
     async with state.proxy() as data:
-        data['time'] = message.text
+        data['time'] = action
         
     client_id = post_client({'name': data['name'], 'phone': data['phone']})
+    print(client_id)
     if client_id:
-        meeting = post_meeting({'date': data['date'], 'time': data['time'], 'client_id': client_id})
+       
+        meeting = post_meeting(
+            {'date': data['date'], 'time': data['time'], 'client_id': client_id})
         
+        print(meeting)
+
         if meeting:
 
             await bot.send_message(
-                    message.chat.id,
+                call.message.chat.id,
+                md.text(
+                    md.text('Спасибо за проявленный интерес к нашей компании ☺️', md.bold(
+                            data['name'])),
+                    md.text('Телефон: ', data['phone']),
+                    md.text('Дата записи: ', data['date']),
+                    md.text('Время записи: ', data['time']),
                     md.text(
-                        md.text('Спасибо за проявленный интерес к нашей компании', md.bold(data['name'])),
-                        md.text('Телефон: ', data['phone']),
-                        md.text('Дата записи: ', data['date']),
-                        md.text('Время записи: ', data['time']),
-                        md.text(
-                            md.bold('В день собеседования, за пол часа до начала, пришлю уведомление')),
-                        sep='\n',
-                    ),
-                    parse_mode=ParseMode.MARKDOWN,
+                        md.bold('В день собеседования, за пол часа до начала, пришлю уведомление 🔔')),
+                    sep='\n',
+                ),
+                parse_mode=ParseMode.MARKDOWN,
             )
-        else: 
-             await bot.send_message(message.chat.id, 'Что то пошло не так, попробуйте еще раз введя /start')
-    
+        else:
+            await bot.send_message(call.message.chat.id, 'Что то пошло не так, попробуйте еще раз 🥺')
+
     await state.finish()
-        
-    
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
